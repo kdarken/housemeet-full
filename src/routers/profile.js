d8cc966500227fc4bcdf1a1463fe845e84775e95
Divineechidume <divineechidume@berkeley.edu>
2020-07-15 17:55:13 -0700

    vsCode prettier reformatted code

--- src/routers/profile.js
+++ src/routers/profile.js
@@ -38,37 +38,33 @@ router.post("/profiles/update/basic", async (req, res) => {
   }
 });
 
-
-router.post("/profiles/update/habits", auth, async (req, res) => {
+router.post("/profiles/update/habits", async (req, res) => {
+  //TODO: add auth, get auth to work
   // Update user profile (living habits)
   try {
     console.log(req.body);
-    const { email, otherInfo } = req.body; //TODO
-    const userProfile = db.user - habits - profiles.find({ email: email });
+    const {
+      email,
+      cleanScore,
+      cleanScore2,
+      guestScore,
+      guestScore2,
+      alcoholScore,
+      alcoholScore2,
+    } = req.body; //TODO; how to clean this up
+    let userProfile = await HabitsProfile.findOne({ email });
+    console.log(userProfile);
     if (!userProfile) {
       userProfile = new HabitsProfile(req.body);
-      //await userProfile.save()
+      userProfile.save();
     } else {
       //TODO
-router.post('/profiles/update/habits', async(req, res) => { //TODO: add auth, get auth to work
-    // Update user profile (living habits)
-    try {
-        console.log(req.body);
-        const { email, cleanScore, cleanScore2, guestScore, guestScore2, alcoholScore, alcoholScore2 } = req.body //TODO; how to clean this up
-        let userProfile = await HabitsProfile.findOne({ email })
-        console.log(userProfile)
-        if (!userProfile) {
-            userProfile = new HabitsProfile(req.body)
-            userProfile.save()
-        } else {
-           //TODO
-        }
-        res.status(201).send()
-    } catch (error) {
-        console.log(error)
-        res.status(400).send(error)
     }
-    
+    res.status(201).send();
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
 });
 
 router.get("/profiles/me", auth, async (req, res) => {
