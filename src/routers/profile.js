fffc06f2362092df04b989f82a0e01f985c173d4
Karen Darken <karen.darken@berkeley.edu>
2020-07-17 10:46:20 -0700

    fixing function that got messed up after merge

--- src/routers/profile.js
+++ src/routers/profile.js
@@ -40,17 +40,6 @@ router.post("/profiles/update/basic", async (req, res) => {
 
 
 router.post("/profiles/update/habits", auth, async (req, res) => {
-  // Update user profile (living habits)
-  try {
-    console.log(req.body);
-    const { email, otherInfo } = req.body; //TODO
-    const userProfile = db.user - habits - profiles.find({ email: email });
-    if (!userProfile) {
-      userProfile = new HabitsProfile(req.body);
-      //await userProfile.save()
-    } else {
-      //TODO
-router.post('/profiles/update/habits', async(req, res) => { //TODO: add auth, get auth to work
     // Update user profile (living habits)
     try {
         console.log(req.body);
@@ -61,14 +50,13 @@ router.post('/profiles/update/habits', async(req, res) => { //TODO: add auth, ge
             userProfile = new HabitsProfile(req.body)
             userProfile.save()
         } else {
-           //TODO
+            //TODO
         }
         res.status(201).send()
     } catch (error) {
         console.log(error)
         res.status(400).send(error)
     }
-    
 });
 
 router.get("/profiles/me", auth, async (req, res) => {
